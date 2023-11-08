3fd4baf4c569bcfe40b39ac2390f1f3e73d39d84
Jan Macku <jamacku@redhat.com>
2023-11-08 15:48:09 +0100

    Add debug logging

--- dist/pull-request.js
+++ dist/pull-request.js
@@ -1,4 +1,4 @@
-import { warning } from '@actions/core';
+import { debug, warning } from '@actions/core';
 import { context } from '@actions/github';
 import { Metadata } from './metadata';
 export class PullRequest {
@@ -14,18 +14,21 @@ export class PullRequest {
         return !!this.metadata.commentID && !!this.metadata.tag;
     }
     isTagPolicyCompliant(tagPolicy, tag) {
+        debug(`Checking tag policy for PR: #${this.id}`);
         const freezingTag = tag !== null && tag !== void 0 ? tag : this.metadata.tag;
         if (freezingTag === undefined)
             false;
         return tagPolicy.some(regex => new RegExp(regex).test(freezingTag));
     }
     async freeze(content, freezingTag) {
+        debug(`Freezing PR: #${this.id}`);
         const id = await this.publishComment(content);
         this.metadata.commentID = id === undefined ? id : id.toString();
         this.metadata.tag = freezingTag;
         await this.metadata.setMetadata();
     }
     async unfreeze(content) {
+        debug(`Unfreezing PR: #${this.id}`);
         const id = await this.publishComment(content);
         this.metadata.commentID = id === undefined ? id : id.toString();
         await this.metadata.setMetadata();
@@ -46,12 +49,14 @@ export class PullRequest {
     async createComment(body) {
         if (!body || body === '')
             return;
+        debug(`Creating comment for PR: #${this.id}`);
         const { data } = await this.octokit.request('POST /repos/{owner}/{repo}/issues/comments', Object.assign(Object.assign({}, context.repo), { issue_number: this.id, body }));
         return data;
     }
     async updateComment(body) {
         if (!this.metadata.commentID)
             return;
+        debug(`Updating comment with ID: ${this.metadata.commentID}`);
         const { data } = await this.octokit.request('PATCH /repos/{owner}/{repo}/issues/comments/{comment_id}', Object.assign(Object.assign({}, context.repo), { comment_id: +this.metadata.commentID, body }));
         return data;
     }
