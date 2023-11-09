2dadf91db413400774ae3b72b5ac0ad89c0ee488
Jan Macku <jamacku@redhat.com>
2023-11-09 12:40:00 +0100

    test: clean tests

--- test/integration/fixtures/metadata.fixture.ts
+++ test/integration/fixtures/metadata.fixture.ts
@@ -4,35 +4,22 @@ import {
   PullRequestSynchronizeEvent,
 } from '@octokit/webhooks-types';
 
-import { events } from '../../../src/events';
-
 import payloadPullRequestOpened from '../fixtures/payloads/pull-request-opened.json';
 import payloadPullRequestReopened from '../fixtures/payloads/pull-request-reopened.json';
 import payloadPullRequestSynchronize from '../fixtures/payloads/pull-request-synchronize.json';
 
 export interface IMetadataTestContext {
-  payloads: {
-    type: (typeof events.pull_request)[number];
-    payload:
-      | PullRequestOpenedEvent
-      | PullRequestReopenedEvent
-      | PullRequestSynchronizeEvent;
-  }[];
+  payloads: (
+    | PullRequestOpenedEvent
+    | PullRequestReopenedEvent
+    | PullRequestSynchronizeEvent
+  )[];
 }
 
 export const metadataContextFixture: IMetadataTestContext = {
   payloads: [
-    {
-      type: 'pull_request.opened',
-      payload: payloadPullRequestOpened as PullRequestOpenedEvent,
-    },
-    {
-      type: 'pull_request.reopened',
-      payload: payloadPullRequestReopened as PullRequestReopenedEvent,
-    },
-    {
-      type: 'pull_request.synchronize',
-      payload: payloadPullRequestSynchronize as PullRequestSynchronizeEvent,
-    },
+    payloadPullRequestOpened as PullRequestOpenedEvent,
+    payloadPullRequestReopened as PullRequestReopenedEvent,
+    payloadPullRequestSynchronize as PullRequestSynchronizeEvent,
   ],
 };
--- test/unit/fixtures/metadata.fixture.ts
+++ test/unit/fixtures/metadata.fixture.ts
@@ -4,7 +4,6 @@ import { Metadata } from '../../../src/metadata';
 
 export interface IMetadataTestContext {
   metadata: Metadata[];
-  invalid: Metadata[];
 }
 
 const controller = {} as MetadataController;
@@ -15,24 +14,5 @@ export const metadataContextFixture: IMetadataTestContext = {
     new Metadata(0, controller, { tag: 'v1', commentID: undefined }),
     new Metadata(0, controller, { tag: undefined, commentID: '123456789' }),
     new Metadata(0, controller, { tag: 'v1', commentID: '123456789' }),
-  ],
-
-  invalid: [
-    // @ts-expect-error: Let's ignore a type error, it's required for testing
-    new Metadata(),
-    // @ts-expect-error: Let's ignore a type error, it's required for testing
-    new Metadata(null),
-    // @ts-expect-error: Let's ignore a type error, it's required for testing
-    new Metadata(undefined),
-    // @ts-expect-error: Let's ignore a type error, it's required for testing
-    new Metadata(''),
-    // @ts-expect-error: Let's ignore a type error, it's required for testing
-    new Metadata({}),
-    // @ts-expect-error: Let's ignore a type error, it's required for testing
-    new Metadata([]),
-    // @ts-expect-error: Let's ignore a type error, it's required for testing
-    new Metadata({ tag: null, commentID: null }),
-    // @ts-expect-error: Let's ignore a type error, it's required for testing
-    new Metadata({ tag: undefined, commentID: 123456789 }),
   ],
 };
