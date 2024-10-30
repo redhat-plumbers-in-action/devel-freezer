78b24e48de44f62f0b661702b760d3ce627f5565
Jan Macku <jamacku@redhat.com>
2024-10-30 08:20:47 +0100

    deps: update dependencies

---
+++ dist/schema/config.js
@@ -0,0 +1,17 @@
+import { z } from 'zod';
+export const policySchema = z.object({
+    tags: z.array(z.string().min(1)),
+    labels: z
+        .object({
+        allow: z.array(z.string().min(1)),
+    })
+        .optional(),
+    feedback: z.object({
+        'frozen-state': z.string().min(1),
+        'unfreeze-state': z.string().min(1),
+    }),
+});
+export const configSchema = z.object({
+    policy: z.array(policySchema).min(1),
+});
+//# sourceMappingURL=config.js.map
